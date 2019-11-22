923c863f8c4f8d6f4997950db7eb6a5d353c01be
mag-da-lenka <magdalenka.ag@gmail.com>
2019-11-22 15:17:40 +0100

    updated with the song exercise

--- javascript/javascript1/week3/script-JS1-3-hw.js
+++ javascript/javascript1/week3/script-JS1-3-hw.js
@@ -1,13 +1,13 @@
-console.log(`HOMEWORK JS1-3 week-07 by Magdalena /HYF /class 12`);
+
+console.log(`Hello World! / Parallel world connection test.`)
+console.log(`This is HOMEWORK JS1-3 week-07 by Magdalena /HYF /class 12.`);
+
+
 
 
 
 // 1. Codecamp -done.  
-console.log(`Hello World! My username on Codecamp is mag-da-lena.`); //parallel world connection test :) 
-
-
-
-
+console.log(`My username on Codecamp is mag-da-lena.`);
 
 
 
@@ -21,6 +21,7 @@ const nameToRemove = 'Ahmad';
 names.splice(1, 1); // replaces 1 element at index 1 with nothingness :>
 console.log(names); // expected:  ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'] 
 //                       result:  ["Peter", "Yana", "kristina", "Rasmus", "Samuel", "katrine", "Tala"]
+
 
 
 
@@ -54,9 +55,7 @@ function howLong() {
 
 };
 
-howLong(travelInformation); //up to this point it will display the above consol.log = in munutes
-
-
+howLong(travelInformation);
 
 
 
@@ -144,6 +143,99 @@ showMeWhatIveDoneWithMyLife(seriesDurations, 39);
 // 5. My favorite songs 
 
 
+let songDatabase = [ //an array of song-objects
+
+     {
+          songId: 1,
+          title: 'Lateralus',
+          artist: 'Tool',
+     },
+
+     {
+          songId: 2,
+          title: 'Dumb Surfer',
+          artist: 'King Krule',
+     },
+
+     {
+          songId: 3,
+          title: 'Evan Finds The Third Room',
+          artist: 'Khruangbin',
+     },
+
+     {
+          songId: 4,
+          title: 'Drunkship of Lanterns',
+          artist: 'The Mars Volta',
+     },
+
+
+];
+
+// console.log(typeof(songDatabase)); // --->  object 
+// console.log(songDatabase); // ---> ---> proto = array, length is 4 
+
+let myPlaylist = [];
+console.log(myPlaylist);
+console.log(` ...so myPlaylist ${myPlaylist} seems to be an empty array now `);
+
+
+
+function addSongToDatabase(song) {
+
+     songDatabase.push(song);
+     return console.log(songDatabase);
+
+     // Q: Should the function return anything? 
+     // A: I think that teachnically it is optional. The songs will be added anyway. Personally I like to have a tool to dynamically control and automatically display my updated song database.  
+
+}
+
+// Before I just used the song-objects it took me some time to figure out how to do it because they didn't have a name I'd be able to use. 3874628746 days later.... :> 
+addSongToDatabase({ songId: 5, title: 'Add This Song', artist: 'GusGus', }); // so I've just put the unnamed object as a parameter ..and it finally works... 
+addSongToDatabase({ songId: 6, title: 'Roots Bloody Roots', artist: 'Sepultura', });
+addSongToDatabase({ songId: 7, title: 'Us Or Them', artist: 'The Cure', });
+// addSongToDatabase({ title: 'Us Or Them', artist: 'The Cure', }); // no songId -- just a test -- it was added anyway :) 
+
+
+
+
+function getSongByTitle(title) {
+
+     for (let i = 0; i < songDatabase.length; i++) {  //for Loop to be able to access the full list of potentially matching songs 
+
+          if (title === songDatabase[i].title) { // if the parameter, eg. 'Lateralus' is exactly the same as any of the values in title keys (= title properties), the function will return the full song-object we're searching for. BTW here the return is a must! 
+               return console.log(songDatabase[i]);
+          }
+
+          if (title !== songDatabase[i].title) { // if the parameter, eg. 'Lateralus' is not the same as any of the values in title keys (= title properties), the function is told to return ---> undefined. 
+               return console.log(undefined);
+          }
+
+     }
+
+}
+
+getSongByTitle('Lateralus'); // the full song-object is displayed, yay! 
+getSongByTitle('Later, Alus!'); // ---> undefined // so it works. 
+
+
+
+
+
+
+function addSongToMyPlaylist(title) {
+
+     console.log(title); // ---> 'Dumb Surfer' 
+     getSongByTitle(title);   //  I wish that worked but it is undefined and the full object containing 'Dumb Surfer' was displayed, can't make it work  
+
+     myPlaylist.push(title); //now I I have pushed the title 'Dumb Surfer' into empty myPlaylist array created before the functions
+     // console.log(myPlaylist); // ---> ['Dumb Surfer'] ---> it works
+
+}
+
+addSongToMyPlaylist('Dumb Surfer');
+console.log(myPlaylist); // ---> ["Dumb Surfer"] // --> the problem here is that it is just the title and not the whole song-object... 
 
 
 
