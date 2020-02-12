18a627545ab478adce132ea8d6777c1317ef564d
mag-da-lenka <51802866+mag-da-lenka@users.noreply.github.com>
2020-02-12 21:16:38 +0100

    Update db3_homework_forest_database_queries.sql

--- databases/week3/db3_homework_forest_database_queries.sql
+++ databases/week3/db3_homework_forest_database_queries.sql
@@ -16,7 +16,7 @@ squirrels.id AS `SQUIRRELS_id`, squirrels.name AS `SQUIRRELS_name`, fur_type, em
 tasks.id AS `TASKS_id`, tasks.title AS `TASKS_title`, tasks.description AS `TASKS_description`, created, starts, ends, 
 tags.id AS TAGS_id, tags.name AS TAGS_name 
 FROM               squirrels JOIN squirrels_tasks   ON squirrels.id               =  squirrels_tasks.squirrel_id 
-			     JOIN tasks             ON squirrels_tasks.task_id    =  tasks.id 
+			       JOIN tasks             ON squirrels_tasks.task_id    =  tasks.id 
                              JOIN tasks_tags        ON tasks_tags.task_id         =  tasks.id 
                              JOIN tags              ON tasks_tags.tag_id          =  tags.id
 ORDER BY squirrels.id; 
@@ -26,7 +26,7 @@ squirrels.id AS `SQUIRRELS_id`, squirrels.name AS `SQUIRRELS_name`, fur_type, em
 tasks.id AS `TASKS_id`, tasks.title AS `TASKS_title`, tasks.description AS `TASKS_description`, created, starts, ends, 
 tags.id AS TAGS_id, tags.name AS TAGS_name 
 FROM               squirrels JOIN squirrels_tasks   ON squirrels.id               =  squirrels_tasks.squirrel_id 
-			     JOIN tasks             ON squirrels_tasks.task_id    =  tasks.id 
+			       JOIN tasks             ON squirrels_tasks.task_id    =  tasks.id 
                              JOIN tasks_tags        ON tasks_tags.task_id         =  tasks.id 
                              JOIN tags              ON tasks_tags.tag_id          =  tags.id
 ORDER BY tasks.id; 
@@ -36,7 +36,7 @@ squirrels.id AS `SQUIRRELS_id`, squirrels.name AS `SQUIRRELS_name`, fur_type, em
 tasks.id AS `TASKS_id`, tasks.title AS `TASKS_title`, tasks.description AS `TASKS_description`, created, starts, ends, 
 tags.id AS TAGS_id, tags.name AS TAGS_name 
 FROM               squirrels JOIN squirrels_tasks   ON squirrels.id               =  squirrels_tasks.squirrel_id 
-			     JOIN tasks             ON squirrels_tasks.task_id    =  tasks.id 
+			       JOIN tasks             ON squirrels_tasks.task_id    =  tasks.id 
                              JOIN tasks_tags        ON tasks_tags.task_id         =  tasks.id 
                              JOIN tags              ON tasks_tags.tag_id          =  tags.id
 ORDER BY tags.id;
