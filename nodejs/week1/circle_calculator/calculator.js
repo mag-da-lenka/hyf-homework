a7b78c419ab24b352d46ec67f9ec8b98f8225a56
mag-da-lenka <magdalenka.ag@gmail.com>
2020-02-28 23:47:33 +0100

    added terminal log

--- nodejs/week1/circle_calculator/calculator.js
+++ nodejs/week1/circle_calculator/calculator.js
@@ -23,13 +23,23 @@ console.log(circle27.getArea());          // 2290.221044466959
 
 
 /*
-Madzik@DESKTOP-7S4P8PM MINGW64 ~/Downloads/1-coding-hyf/hyf-12-homework-FINAL/hyf-homework/nodejs/week1/circle_calculator (master)
-$ node calculator.js
-20
+Madzik@DESKTOP-7S4P8PM MINGW64 ~/Downloads/1-coding-hyf/hyf-12-homework-FINAL/hyf-homework/nodejs/week1/circle_calculator (nodejs_week1_homework)
+$ npm run dev
+
+> circle_calculator@1.0.0 dev C:\Users\Madzik\Downloads\1-coding-hyf\hyf-12-homework-FINAL\hyf-homework\nodejs\week1\circle_calculator
+> nodemon calculator.js
+
+[nodemon] 2.0.2
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching dir(s): *.*
+[nodemon] watching extensions: js,mjs,json  
+[nodemon] starting `node calculator.js`     
+^S20
 62.83185307179586
 314.1592653589793
 54
 169.64600329384882
-2290.221044466959 
+2290.221044466959
+[nodemon] clean exit - waiting for changes before restart   
 */
 
